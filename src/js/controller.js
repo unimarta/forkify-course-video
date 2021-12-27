671d2f8ef680a20edc4d974bff50d4288c8994ea
unimarta <kramkagitarzystka@yahoo.pl>
2021-12-27 20:43:01 +0100

    Added new welcome feature

--- src/js/controller.js
+++ src/js/controller.js
@@ -120,7 +120,9 @@ const controlAddRecipe = async function (newRecipe) {
     addRecipeView.renderError(err.message);
   }
 };
-
+const newFeature = function () {
+  console.log('Welcome to the application');
+};
 const init = function () {
   bookmarksView.addHandlerRender(controlBookmarks);
   recipeView.addHandlerRender(controlRecipes);
@@ -129,6 +131,6 @@ const init = function () {
   searchView.addHandlerSearch(controlSearchResults);
   paginationView.addHandlerClick(controlPagination);
   addRecipeView.addHandlerUpload(controlAddRecipe);
-  console.log('Welcome!');
+  newFeature();
 };
 init();
